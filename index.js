96fdbd9ac6f7135323bf7c2ec0eff163b0882514
Pikarestiv <okwyodumodu@gmail.com>
2021-01-08 13:45:49 +0100

    Got csv from url in the body of the post request

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const port = process.env.port || 3000;
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
 
 const app = express();
+
+app.use(bodyParser.json());
+
 app.listen(port, "127.0.0.1", () => {
   console.log(`Server Running On ${port}`);
 });
@@ -9,4 +14,12 @@ app.listen(port, "127.0.0.1", () => {
 app.get("/", (req, res) => {
   console.log("Hello World!");
   res.send("<h1>Hello World!</h1>");
+})
+
+app.post("/api", (req,res) => {
+  fetch(req.body.csv.url)
+    .then(res => res.text())
+    .then((file) => {
+      console.log(file);
+    })
 })
