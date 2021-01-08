98833f3b4192abb12fea71fe717548d6b834b7dd
Pikarestiv <okwyodumodu@gmail.com>
2021-01-08 13:49:24 +0100

    Installed CSV File Validator

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const port = process.env.port || 3000;
 const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
+const CSVFileValidator = require("csv-file-validator");
 
 const app = express();
 
