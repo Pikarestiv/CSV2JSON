9e9a016c9338e27ea2f420519f3aaf7c7f2a9db5
Pikarestiv <okwyodumodu@gmail.com>
2021-01-08 15:44:26 +0100

    More validation

--- server.js
+++ server.js
@@ -13,38 +13,55 @@ app.listen(port, "127.0.0.1", () => {
 });
 
 app.get("/", (req, res) => {
-  console.log("Hello World!");
-  res.send("<h1>Hello World!</h1>");
-})
+  console.log("CSV2JSON");
+  res.send("<h1>CSV2JSON</h1>");
+});
 
-app.post("/api", (req,res) => {
+let randomString = (length, chars) => {
+  var result = "";
+  for (var i = length; i > 0; --i)
+    result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+};
+
+app.post("/api", (req, res) => {
   fetch(req.body.csv.url)
-    .then(res => res.text())
-    .then(file => {
-      const selFields = req.body.csv.select_fields;
+    .then((res) => res.text())
+    .then((file) => {
       let config = {
         headers: [],
-        isHeaderNameOptional: false
+        isHeaderNameOptional: true,
       };
-      
-      selFields.forEach(field => {
-        config.headers.push(
-          {
+
+      //Only Parse in parameters into headers array if parameters exist
+      if (req.body.csv.select_fields) {
+        const selFields = req.body.csv.select_fields;
+
+        selFields.forEach((field) => {
+          config.headers.push({
             name: field,
             inputName: field,
-            required: true
-          }
-        );
-      });
+            required: true,
+          });
+        });
+
+        config.isHeaderNameOptional = false;
+      }
 
       CSVFileValidator(file, config)
         .then((csvData) => {
-          // console.log(csvData.inValidMessages);
-          res.send(csvData.data);
+          console.log(csvData.inValidMessages);
+          res.send({
+            conversion_key: randomString(
+              32,
+              "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            ),
+            json: csvData.data,
+          });
         })
         .catch((err) => {
-          // console.log(csvData.inValidMessages);
+          console.log(csvData.inValidMessages);
           res.send(csvData.inValidMessages);
         });
-    })
-})
+    });
+});
